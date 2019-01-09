f902b25fa026cd2e3ef5be1e124123db8860060e
Daler Norkulov <dalernorkulov@gmail.com>
2019-01-09 12:41:55 -0500

    Revert "Removed ".gradle" from gitignore folder"
    This reverts commit 73550e10a766e021c017242358c873e7f04f3495.

--- Product/app/src/main/java/com/nyit/seniordesignproject/conciousgps/DevelopmentTesting.java
+++ Product/app/src/main/java/com/nyit/seniordesignproject/conciousgps/DevelopmentTesting.java
@@ -1,29 +1,14 @@
 package com.nyit.seniordesignproject.conciousgps;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
 public class DevelopmentTesting extends AppCompatActivity {
-int buttonCounter = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.development_testing);
-
-        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                buttonCounter++;
-                if(buttonCounter >= 5)  {
-                    buttonCounter = 0;
-                    //setContentView(R.layout.development_testing);
-                    Intent nextScreen = new Intent(getApplicationContext(), MainActivity.class);
-                    startActivity(nextScreen);
-                }
-            }
-        });
 
 
     }
